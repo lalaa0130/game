4e93f561f68cac22c5f73b82e6cf16bb3ba47fa7
lalaa0130 <115624308+lalaa0130@users.noreply.github.com>
2023-09-02 15:28:25 +0900

    Add files via upload

---
+++ quine/quine.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+ 
+int main()
+{
+   const char *str = "int main()%c{%c%cconst char *str = %c%s%c%c%cprintf(str,10,10,9,34, str,34,10,9,10,10);%c}%c";
+   printf(str,10,10,9,34, str,34,10,9,10,10);
+}
+
